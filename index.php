26b47d7544e143dbcab431fa7417e273c02055a1
YuliakK714 <63894707+YuliakK714@users.noreply.github.com>
2020-04-18 10:45:27 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include('Templates/_config.php');
+?>
+<?
+$pageTitle = 'Главная страница';
+?>
+<?php
+include('Templates/_head.php');
+?>
+<?php
+include('Templates/_header.php');
+?>
+	<div class = "Content1"> 
+		<?php
+		include('Templates/_content1.php');
+		?>
+	</div> 
+	<div class = "Content2"> 
+		<?php
+		include('Templates/_content2.php');
+		?>
+	</div>
+	<div class="fonn">
+	<div class="cl3">
+		<img src="img/Frame21.png" class="fon2">
+	
+	</div>
+</div>	
+	<div class="bll" >
+		<?php
+		include('Templates/_content4.php');
+		?>
+	</div>
+	<div class="color">
+		<?php
+		include('Templates/_content5.php');
+		?>
+	</div>
+</div>
+<?php
+include('Templates/_footer.php');
+?>
